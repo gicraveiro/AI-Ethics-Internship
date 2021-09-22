3f5fd61cc6b1d61944675fd237d293170c08c480
gicraveiro <gmcraveiro@hotmail.com>
2021-09-22 12:19:14 +0200

    calculated frequency of terms in cookies policy and size of lexicon test successful

--- corpus_stats.py
+++ corpus_stats.py
@@ -1,6 +1,9 @@
 import pdfx
+import spacy
+import nltk
 
 # TRANSFORMING ALL PDFS INTO TEXT
+
 cookies_policy = pdfx.PDFx('data/Facebook/TargetCompanySourced/CookiesPolicy.pdf')
 cookies_policy = cookies_policy.get_text()
 #print(cookies_policy)
@@ -19,4 +22,19 @@ open_source_privacy_policy = open_source_privacy_policy.get_text()
 
 terms_of_service = pdfx.PDFx('data/Facebook/TargetCompanySourced/TermsOfService.pdf')
 terms_of_service = terms_of_service.get_text()
-print(terms_of_service)
+#print(terms_of_service)
+
+# COMPUTING STATS
+
+nlp = spacy.load('en_core_web_sm')
+
+doc_cookies = nlp(cookies_policy)
+tokens = [t for t in doc_cookies.text.split()]
+
+freq = nltk.FreqDist(tokens)
+print('Size of Lexicon:' + len(freq))
+i = 0
+for key, val in freq.items():
+    print(str(key) + ':' + str(val))
+    i +=1
+print(i)
