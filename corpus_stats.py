f8c801d8f4dcc569ad5ea956e23ba2ace96aac1d
gicraveiro <gmcraveiro@hotmail.com>
2021-09-22 17:01:42 +0200

    moved output from terminal to an output file

--- corpus_stats.py
+++ corpus_stats.py
@@ -8,7 +8,7 @@ import nltk
 def nbest(d, n=5):
     """
     get n max values from a dict
-    :param d: input dict (values are numbers, keys are stings)
+    :param d: input dict (values are numbers, keys are strings)
     :param n: number of values to get (int)
     :return: dict of top n key-value pairs
     """
@@ -18,59 +18,82 @@ def compute_stats(file):
 
     doc = nlp(file)
     tokens = [t for t in doc.text.split()]
+    keywords_present = []
 
     freq = nltk.FreqDist(tokens)
-    print('Total number of tokens:', len(tokens))
+    print('Total number of tokens:', len(tokens), file=output_file)
     # TO DO: TOTAL NUMBER OF UTTERANCES (SENTENCES)
-    print('Size of Lexicon:', len(freq))
+    print('Size of Lexicon:', len(freq), file=output_file)
+    freq.plot(20, cumulative=False) # TO DO: ADD GRAPHS TO FILE
 
-    #print(freq.keys())
+    freq = nbest(freq,len(freq)) 
+
+    print('\nTokens that appear in the file, alongside with their frequencies:', file=output_file)
     for key, val in freq.items():
-        #print(str(key) + ':' + str(val))
+        print(str(key) + ':' + str(val), file=output_file)
         for keyword in keywords:
             if (keyword.lower() == key.lower()):
-                print(str(key) + ':' + str(val))
+                keywords_present.append(str(key) + ':' + str(val))
 
-    freq.plot(20, cumulative=False)
-    print(nbest(freq,len(freq)))
+    print("\nKeywords that appear in the file, alongside with their frequencies:", file=output_file)
+    for keyword in keywords_present:
+        print(keyword, file=output_file)
+    
+#####
+#  MAIN 
+#####
 
 nlp = spacy.load('en_core_web_sm')
 
+# CREATING OUTPUT FILE
+
+output_file = open('output/PrivacyFacebookSourcedStats.txt', 'w')
+
 # KEYWORDS 
 
-keywords = open('data/Keywords/Privacy KeyWords Formatted.docx.txt', "r", encoding='utf-8-sig')
-keywords = keywords.read()
+keywords_file = open('data/Keywords/Privacy KeyWords Formatted.docx.txt', "r", encoding='utf-8-sig')
+keywords = keywords_file.read()
 keywords = keywords.split(", ")
-print(keywords)
+keywords_file.close()
+print("Descriptive Statistics of Facebook Sourced Files on Privacy", file=output_file)
+print("\nKeywords:\n",file=output_file)
+for keyword in keywords:
+    print(keyword, file=output_file)
 
-# TRANSFORMING ALL PDFS INTO TEXT
+
+# TRANSFORMING ALL PDFS INTO TEXT AND COMPUTING STATS
 
 cookies_policy = pdfx.PDFx('data/Facebook/TargetCompanySourced/CookiesPolicy.pdf')
 cookies_policy = cookies_policy.get_text()
 #print(cookies_policy)
-print("\nCookie Policy Stats\n")
+print("\nCookie Policy Stats\n", file=output_file)
 compute_stats(cookies_policy)
 
 data_policy = pdfx.PDFx('data/Facebook/TargetCompanySourced/DataPolicy.pdf')
 data_policy = data_policy.get_text()
 #print(data_policy)
-print("\nData Policy Stats\n")
+print("\nData Policy Stats\n", file=output_file)
 compute_stats(data_policy)
 
 gen_info_privacy = pdfx.PDFx('data/Facebook/TargetCompanySourced/General Info ProtectingPrivacyAndSecurity.pdf')
 gen_info_privacy = gen_info_privacy.get_text()
 #print(gen_info_privacy)
-print("\nGeneral Info on Privacy Stats\n")
+print("\nGeneral Info on Privacy Stats\n", file=output_file)
 compute_stats(gen_info_privacy)
 
 open_source_privacy_policy = pdfx.PDFx('data/Facebook/TargetCompanySourced/OpenSourcePrivacyPolicy.pdf')
 open_source_privacy_policy = open_source_privacy_policy.get_text()
 #print(open_source_privacy_policy)
-print("\nOpen Source Privacy Policy Stats\n")
+print("\nOpen Source Privacy Policy Stats\n", file=output_file)
 compute_stats(open_source_privacy_policy)
 
 terms_of_service = pdfx.PDFx('data/Facebook/TargetCompanySourced/TermsOfService.pdf')
 terms_of_service = terms_of_service.get_text()
 #print(terms_of_service)
-print("\nTerms of Service Stats\n")
+print("\nTerms of Service Stats\n", file=output_file)
 compute_stats(terms_of_service)
+
+# TO DO: REMOVE STOP WORDS FUNCTION
+# TO DO: SEPARATE OUTPUT OF EACH FILE INTO ITS OWN OUTPUT FILE 
+
+output_file.close()
