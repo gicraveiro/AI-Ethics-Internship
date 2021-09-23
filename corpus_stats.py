7c380ddc1019c5f49e22a46015e4c382f14d4093
gicraveiro <gmcraveiro@hotmail.com>
2021-09-23 12:37:25 +0200

    output in different files

--- corpus_stats.py
+++ corpus_stats.py
@@ -15,7 +15,10 @@ def nbest(d, n=5):
     """
     return dict(sorted(d.items(), key=lambda item: item[1], reverse=True)[:n])
 
-def compute_stats(file,filename):
+def compute_stats(file,filename, path):
+   
+    output_file = open(path, 'w')
+    print("\n"+filename+"\n", file=output_file)
 
     doc = nlp(file)
     tokens = [t for t in doc.text.split()]
@@ -44,6 +47,8 @@ def compute_stats(file,filename):
     for keyword in keywords_present:
         print(keyword, file=output_file)
     
+    output_file.close()
+    
 #####
 #  MAIN 
 #####
@@ -52,14 +57,15 @@ nlp = spacy.load('en_core_web_sm')
 
 # CREATING OUTPUT FILE
 
-output_file = open('output/PrivacyFacebookSourcedStats.txt', 'w')
+#output_file = open('output/PrivacyFacebookSourcedStats.txt', 'w')
 
 # KEYWORDS 
-
+output_file = open('output/Privacy/Keywords.txt', 'w')
 keywords_file = open('data/Keywords/Privacy KeyWords Formatted.docx.txt', "r", encoding='utf-8-sig')
 keywords = keywords_file.read()
 keywords = keywords.split(", ")
 keywords_file.close()
+
 print("Descriptive Statistics of Facebook Sourced Files on Privacy", file=output_file)
 print("\nKeywords:\n",file=output_file)
 for keyword in keywords:
@@ -71,34 +77,34 @@ for keyword in keywords:
 cookies_policy = pdfx.PDFx('data/Facebook/TargetCompanySourced/CookiesPolicy.pdf')
 cookies_policy = cookies_policy.get_text()
 #print(cookies_policy)
-print("\nCookie Policy Stats\n", file=output_file)
-compute_stats(cookies_policy, 'CookiesPolicy')
+#print("\nCookie Policy Stats\n", file=output_file)
+compute_stats(cookies_policy, 'CookiesPolicy', 'output/Privacy/FacebookSourced/CookiesPolicy.txt')
 
 data_policy = pdfx.PDFx('data/Facebook/TargetCompanySourced/DataPolicy.pdf')
 data_policy = data_policy.get_text()
 #print(data_policy)
-print("\nData Policy Stats\n", file=output_file)
-compute_stats(data_policy,'DataPolicy')
+#print("\nData Policy Stats\n", file=output_file)
+compute_stats(data_policy,'DataPolicy', 'output/Privacy/FacebookSourced/DataPolicy.txt')
 
 gen_info_privacy = pdfx.PDFx('data/Facebook/TargetCompanySourced/General Info ProtectingPrivacyAndSecurity.pdf')
 gen_info_privacy = gen_info_privacy.get_text()
 #print(gen_info_privacy)
-print("\nGeneral Info on Privacy Stats\n", file=output_file)
-compute_stats(gen_info_privacy, 'General Info ProtectingPrivacyAndSecurity')
+#print("\nGeneral Info on Privacy Stats\n", file=output_file)
+compute_stats(gen_info_privacy, 'General Info ProtectingPrivacyAndSecurity', 'output/Privacy/FacebookSourced/General Info ProtectingPrivacyAndSecurity.txt')
 
 open_source_privacy_policy = pdfx.PDFx('data/Facebook/TargetCompanySourced/OpenSourcePrivacyPolicy.pdf')
 open_source_privacy_policy = open_source_privacy_policy.get_text()
 #print(open_source_privacy_policy)
-print("\nOpen Source Privacy Policy Stats\n", file=output_file)
-compute_stats(open_source_privacy_policy, 'OpenSourcePrivacyPolicy')
+#print("\nOpen Source Privacy Policy Stats\n", file=output_file)
+compute_stats(open_source_privacy_policy, 'OpenSourcePrivacyPolicy', 'output/Privacy/FacebookSourced/OpenSourcePrivacyPolicy.txt')
 
 terms_of_service = pdfx.PDFx('data/Facebook/TargetCompanySourced/TermsOfService.pdf')
 terms_of_service = terms_of_service.get_text()
 #print(terms_of_service)
-print("\nTerms of Service Stats\n", file=output_file)
-compute_stats(terms_of_service, 'TermsOfService')
+#print("\nTerms of Service Stats\n", file=output_file)
+compute_stats(terms_of_service, 'TermsOfService', 'output/Privacy/FacebookSourced/TermsOfService.txt')
 
 # TO DO: REMOVE STOP WORDS FUNCTION
 # TO DO: SEPARATE OUTPUT OF EACH FILE INTO ITS OWN OUTPUT FILE 
 
-output_file.close()
+#output_file.close()
