605280f42af468d0dd1b8f0c5ef57a04e0bd9c68
gicraveiro <gmcraveiro@hotmail.com>
2021-11-04 12:04:23 +0100

    now writing file with json library instead of printing

--- partition.py
+++ partition.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 from sklearn.model_selection import train_test_split
 import os
-
+import json
 annotation = pd.read_csv("data/Facebook/Privacy/Annotated/fbAnnotation.csv")
 sents = annotation['Sentences'].values
 labels = annotation['Labels'].values
@@ -13,29 +13,29 @@ sents_train, sents_test, labels_train, labels_test = train_test_split(sents,labe
 train_dict = []
 for row_id,row in enumerate(sents_train):
     row = re.sub("\n", " ", row)
-    train_dict.append({"text":row, "label":labels_train[row_id]})
+    train_dict.append({"text":row.strip(), "label":labels_train[row_id]})
 
 sents_dev, sents_test, labels_dev, labels_test = train_test_split(sents_test,labels_test, test_size=0.5, stratify=labels_test)
 
 dev_dict = []
 for row_id,row in enumerate(sents_dev):
     row = re.sub("\n", " ", row)
-    dev_dict.append({"text":row, "label":labels_dev[row_id]})
+    dev_dict.append({"text":row.strip(), "label":labels_dev[row_id]})
 test_dict = []
 for row_id,row in enumerate(sents_test):
     row = re.sub("\n", " ", row)
-    test_dict.append({"text":row, "label":labels_test[row_id]})
+    test_dict.append({"text":row.strip(), "label":labels_test[row_id]})
 
 # create output files
 path = 'output/partition/fbdata_train.txt'
 os.makedirs(os.path.dirname(path), exist_ok=True)
 with open('output/partition/fbdata_train.txt', 'w') as train_file:
-    print(train_dict, file=train_file)
+    train_file.write(json.dumps(train_dict, indent=4, ensure_ascii=False))
 path = 'output/partition/fbdata_dev.txt'
 os.makedirs(os.path.dirname(path), exist_ok=True)
 with open('output/partition/fbdata_dev.txt', 'w') as dev_file:
-    print(dev_dict, file=dev_file)
+    dev_file.write(json.dumps(dev_dict, indent=4, ensure_ascii=False))
 path = 'output/partition/fbdata_test.txt'
 os.makedirs(os.path.dirname(path), exist_ok=True)
 with open('output/partition/fbdata_test.txt', 'w') as test_file:
-    print(test_dict, file=test_file)
+    test_file.write(json.dumps(test_dict, indent=4, ensure_ascii=False))
