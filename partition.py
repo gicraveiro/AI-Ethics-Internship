5e02823c9899bca4b28aeae748b12772db9cb76e
gicraveiro <gmcraveiro@hotmail.com>
2021-11-04 11:30:54 +0100

    created dev, test and train files stratified

--- partition.py
+++ partition.py
@@ -1,11 +1,46 @@
 # importing the dataset
 import pandas as pd
+import re
 import matplotlib.pyplot as plt
-housing = pd.read_csv("data/Facebook/Privacy/Annotated/DataPolicy.csv")
-# looking at the dataset
-print(housing.head())
-print(housing.info())
-#print(housing.ocean_proximity.value_counts())
-#print(housing.describe())
+from sklearn.model_selection import train_test_split
+annotation = pd.read_csv("data/Facebook/Privacy/Annotated/fbAnnotation.csv")
+import os
 
-#train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+sents = annotation['Sentences'].values
+labels = annotation['Labels'].values
+sents_train, sents_test, labels_train, labels_test = train_test_split(sents,labels, test_size=0.2, stratify=labels)
+#print(len(sents_train), len(sents_test), len(labels_train), len(labels_test))
+# save a json, separate labels and sents, use a dictionary in python
+train_dict = []
+for row_id,row in enumerate(sents_train):
+    row = re.sub("\n", " ", row)
+    train_dict.append({"text":row, "label":labels_train[row_id]})
+
+#print(len(sents_train), len(sents_test), len(labels_train), len(labels_test))
+sents_dev, sents_test, labels_dev, labels_test = train_test_split(sents_test,labels_test, test_size=0.5, stratify=labels_test)
+
+dev_dict = []
+for row_id,row in enumerate(sents_dev):
+    row = re.sub("\n", " ", row)
+    dev_dict.append({"text":row, "label":labels_dev[row_id]})
+test_dict = []
+for row_id,row in enumerate(sents_test):
+    row = re.sub("\n", " ", row)
+    test_dict.append({"text":row, "label":labels_test[row_id]})
+# create output files
+path = 'output/partition/fbdata_train.txt'
+os.makedirs(os.path.dirname(path), exist_ok=True)
+with open('output/partition/fbdata_train.txt', 'w') as train_file:
+    print(train_dict, file=train_file)
+path = 'output/partition/fbdata_dev.txt'
+os.makedirs(os.path.dirname(path), exist_ok=True)
+with open('output/partition/fbdata_dev.txt', 'w') as dev_file:
+    print(dev_dict, file=dev_file)
+path = 'output/partition/fbdata_test.txt'
+os.makedirs(os.path.dirname(path), exist_ok=True)
+with open('output/partition/fbdata_test.txt', 'w') as test_file:
+    print(test_dict, file=test_file)
+
+
+
+# json library -- delete all \n
