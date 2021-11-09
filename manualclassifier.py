23d491e085340f63ddfe3d9da70690e96deef018
gicraveiro <gmcraveiro@hotmail.com>
2021-11-09 14:28:31 +0100

    managed to read json

--- manualclassifier.py
+++ manualclassifier.py
@@ -1,5 +1,13 @@
-path = 'output/partition/fbdata_train.txt'
-with open(path) as f:
-    document = f.read()
+import json
 
-print(document)
+path = 'output/partition/fbdata_train.json'
+
+json_sentences = []
+
+with open(path) as file:
+    document = file.read()
+    json_sentences = json.loads(document)
+
+for sentence in json_sentences:
+    #print(sentence)
+    print(sentence['text'])
