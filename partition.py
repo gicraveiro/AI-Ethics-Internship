14dccb9d875a041202472f4036283b2d5c64d6cf
gicraveiro <gmcraveiro@hotmail.com>
2021-11-18 15:54:21 +0100

    multilabel partition performed with success, missing statistics only

--- partition.py
+++ partition.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 import os
 import json
 from collections import Counter
-import math
+#import math
 #annotation = pd.read_csv("data/Facebook/Privacy/Annotated/fbAnnotation.csv")
 annotation = pd.read_csv("data/Facebook/Privacy/Annotated/AnnotatedMultiLabelDataset.csv")
 sents = annotation['Sentences'].values
@@ -39,36 +39,38 @@ for row_id,row in enumerate(sents_test):
     row = re.sub("\n", " ", row)
     test_dict.append({"text":row.strip(), "label":labels_test[row_id]})
 
-'''
+#print("DEBUG TEST")
+
 # create output files
-path = 'output/partition/fbdata_train.txt'
+path = 'output/partition/multilabeldata_train.txt'
 os.makedirs(os.path.dirname(path), exist_ok=True)
-with open('output/partition/fbdata_train.txt', 'w') as train_file:
+with open('output/partition/multilabeldata_train.txt', 'w') as train_file:
     train_file.write(json.dumps(train_dict, indent=4, ensure_ascii=False))
-path = 'output/partition/fbdata_dev.txt'
+path = 'output/partition/multilabeldata_dev.txt'
 os.makedirs(os.path.dirname(path), exist_ok=True)
-with open('output/partition/fbdata_dev.txt', 'w') as dev_file:
+with open('output/partition/multilabeldata_dev.txt', 'w') as dev_file:
     dev_file.write(json.dumps(dev_dict, indent=4, ensure_ascii=False))
-path = 'output/partition/fbdata_test.txt'
+path = 'output/partition/multilabeldata_test.txt'
 os.makedirs(os.path.dirname(path), exist_ok=True)
-with open('output/partition/fbdata_test.txt', 'w') as test_file:
+with open('output/partition/multilabeldata_test.txt', 'w') as test_file:
     test_file.write(json.dumps(test_dict, indent=4, ensure_ascii=False))
 
-with open('output/partition/fbdata_train.txt', 'r') as train_file:
+'''
+with open('output/partition/multilabeldata_train.txt', 'r') as train_file:
     json_obj_train = json.loads(train_file.read())
     train_count = []
     for i in json_obj_train:
         item = i['label']
         train_count.append(item)
     print(Counter(train_count))
-with open('output/partition/fbdata_test.txt', 'r') as test_file:
+with open('output/partition/multilabeldata_test.txt', 'r') as test_file:
     json_obj_test = json.loads(test_file.read())
     test_count = []
     for i in json_obj_test:
         item = i['label']
         test_count.append(item)
     print(Counter(test_count))
-with open('output/partition/fbdata_dev.txt', 'r') as dev_file:
+with open('output/partition/multilabeldata_dev.txt', 'r') as dev_file:
     json_obj_dev = json.loads(dev_file.read())
     dev_count = []
     for i in json_obj_dev:
