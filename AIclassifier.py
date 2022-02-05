cb5c24b8df2b272eeb9e5d1c6e31e6ed684d1854
gicraveiro <gmcraveiro@hotmail.com>
2022-02-05 17:36:36 +0100

    ran bigrams with  lr 0.5 and nest 100 multi hot

--- AIclassifier.py
+++ AIclassifier.py
@@ -57,7 +57,7 @@ def format_sentVector_to_SparseMatrix(vectors_list, dictionary):
         counts = Counter(sent_vector)
         for index, freq in counts.items():
             if len(counts.items()) > 0:
-                sparse_vector[index] = freq/len(sent_vector) # 1 DIFFERENT CONFIGURATION POSSIBILITIES
+                sparse_vector[index] = 1 #freq/len(sent_vector) #  DIFFERENT CONFIGURATION POSSIBILITIES
         if (i == 0): # TO DO: OPTIMIZE, NO NEED TO CHECK THIS EVERY TURN
             matrix_array = [sparse_vector]
         else:
